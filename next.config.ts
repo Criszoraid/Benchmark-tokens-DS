a0125248f879da4123558df0e3ead63daec04687
Criszoraid <criszoraid@gmail.com>
2026-02-07 20:59:30 +0100

    Update next.config.ts with new configuration on 2026-02-07 19:59:16 UTC

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Benchmark-tokens-DS",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
